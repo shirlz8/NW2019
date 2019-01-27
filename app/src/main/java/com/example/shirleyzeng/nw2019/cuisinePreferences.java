40e1a80beb890747df0b6693ef033cccfd83ae4c
Shirley Zeng <shirleyzeng8y@gmail.com>
2019-01-26 21:54:18 -0800

    Initial commit

---
+++ app/src/main/java/com/example/shirleyzeng/nw2019/cuisinePreferences.java
@@ -0,0 +1,17 @@
+package com.example.shirleyzeng.nw2019;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cuisinePreferences extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuisine_preferences);
+    }
+
+   // private boolean chineseSelected(){
+
+
+}
