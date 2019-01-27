f056a8b74b4f1154e3e73cb92362b342822e2705
Shirley Zeng <shirleyzeng8y@gmail.com>
2019-01-26 23:40:36 -0800

    Working on adding more screens

--- app/src/main/java/com/example/shirleyzeng/nw2019/cuisinePreferences.java
+++ app/src/main/java/com/example/shirleyzeng/nw2019/cuisinePreferences.java
@@ -1,7 +1,11 @@
 package com.example.shirleyzeng.nw2019;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 public class cuisinePreferences extends AppCompatActivity {
 
@@ -11,7 +15,13 @@ public class cuisinePreferences extends AppCompatActivity {
         setContentView(R.layout.activity_cuisine_preferences);
     }
 
-   // private boolean chineseSelected(){
+   public boolean vegetarianSelected(View view){
+       CheckBox veg = findViewById(R.id.checkBox_Vegetarian);
+       return veg.isChecked();
+       }
 
-
+    public void goToHome (View view){
+        Intent i = new Intent(cuisinePreferences.this, groupSideBarMenu.class);
+        startActivity(i);
+    }
 }
